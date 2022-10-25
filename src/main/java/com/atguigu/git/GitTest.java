5c773f552336a2c7276764e06f8b6b256015529a
mh <2622693924@qq.com>
2022-10-25 10:57:00 +0800

    master commit3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
         System.out.println("hello git");
+        System.out.println("master git");
     }
 }
