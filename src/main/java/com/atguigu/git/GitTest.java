20c761fdce6eb8a2de00c31c407132be8fb16364
mh <2622693924@qq.com>
2022-10-25 10:20:15 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author m
+ * @Date 2022/10/25 10:07
+ * @Version 1.0
+ **/
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
